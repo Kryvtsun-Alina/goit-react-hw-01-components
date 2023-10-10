2932f35cb68580f9845f84fffa2fa9acca38fe16
Kryvtsun Alina <krivtsun1002@gmail.com>
2023-10-10 18:32:16 +0300

    fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -7,15 +7,15 @@ export const FriendList = ({friends}) => {
     return (
         <ul className={css.friendList}>
             {friends.map(
-                ({id, avatar, name, isOnline}) => (<FriendListItem
+              ({ id, avatar, name, isOnline }) => (
+                <FriendListItem
                     key={id}
                     avatar={avatar}
                     name={name}
                     isOnline={isOnline} />)
-        )}
+            )}
         </ul>
-    )
-}
+    )}
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
